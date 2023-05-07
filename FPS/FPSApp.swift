39756217f9e6216b84565a9850c845be332ee330
Almayo Ibrahim <almayo.online@gmail.com>
2023-05-07 12:15:06 +0300

    Initial Commit

---
+++ FPS/FPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FPSApp.swift
+//  FPS
+//
+//  Created by almayo ibrahim  on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
