304b796643f1dd9dba20a464de69486ff0d56797
Almayo Ibrahim <almayo.online@gmail.com>
2023-05-07 15:53:43 +0300

    1. add category grid view file 2. created new group for ViewModels 3. created new group for Models

---
+++ FPS/Views/Home Page/CategoryGrid.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryGrid.swift
+//  FPS
+//
+//  Created by almayo ibrahim  on 07/05/2023.
+//
+
+import SwiftUI
+
+struct CategoryGrid: View {
+
+    var body: some View {
+        
+        let columns = Array(
+            repeating: GridItem(.flexible(), spacing: 10), count: 2
+        )
+
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach(0..<10) { _ in
+                    Color.blue
+                        .frame(height: 200)
+                }
+            }
+            .padding(.horizontal, 10)
+        }
+    }
+}
+
+
+struct CategoryGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryGrid()
+    }
+}
